0e87131a8d64068b72abcc2dcd13b0bed8f3698d
martinfjeld <martinfjeld@live.no>
2021-08-31 16:22:37 +0200

    Added style variables and header components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
-import { Header } from "./components/typography/header/header.component";
+import "./styles/style-variables.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1
+        style={{ color: "var(--primary-color)", fontSize: "var(--large-text)" }}
+      >
+        Hello
+      </h1>
+    </div>
+  );
 }
 
 export default App;
